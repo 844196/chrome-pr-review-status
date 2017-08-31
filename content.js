eee2dd4b0fe5da60bb7340f292cd004d3dc04691
Masaya Takeda <844196@gmail.com>
2017-08-31 12:10:55 +0900

    add toggle status display button
    default: hidden

--- content.js
+++ content.js
@@ -12,6 +12,7 @@
 
         let reviewers = page.querySelector('.discussion-sidebar-item')
         reviewers.classList.add('review-status', 'float-left', 'col-3', 'p-2')
+        reviewers.style.display = 'none'
 
         let prInfo = row.querySelector('.col-9')
         prInfo.classList.remove('col-9')
@@ -21,5 +22,36 @@
       })
   })
 
-  Promise.all(fetchReviewStatusList)
+  let existsBtn = document.getElementById('status-display-toggle-btn')
+  if (existsBtn) {
+    existsBtn.parentNode.removeChild(existsBtn)
+  }
+
+  let btn = document.createElement('button')
+  btn.id = 'status-display-toggle-btn'
+  btn.classList.add('btn', 'btn-default', 'float-right', 'mr-2')
+  btn.textContent = 'Fetching...'
+  btn.dataset.isActive = false
+  btn.setAttribute('disabled', true)
+  document.querySelector('.subnav').appendChild(btn)
+
+  Promise
+    .all(fetchReviewStatusList)
+    .then(() => {
+      btn.removeAttribute('disabled')
+      btn.innerText = 'Toggle review status display'
+    })
+    .then(() => {
+      document
+        .getElementById('status-display-toggle-btn')
+        .addEventListener('click', function () {
+          const isActive = this.dataset.isActive == 'true'
+
+          document.querySelectorAll('.review-status').forEach((item) => {
+            item.style.display = isActive ? 'none' : 'block'
+          })
+
+          this.dataset.isActive = !isActive
+        })
+    })
 })()
