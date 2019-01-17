9fc2d6bfcceb85182cf34307b9757d6e5441e833
Masaya Takeda <844196@gmail.com>
2019-01-18 06:34:24 +0900

    refactor fetch review status func

--- src/external/review.ts
+++ src/external/review.ts
@@ -1,8 +1,15 @@
+import { lookup, StrMap } from 'fp-ts/lib/StrMap';
 import { Review, ReviewCollection, ReviewResult } from '../domain/review';
-import { Reviewer } from '../domain/reviewer';
 import { h } from '../util/create-element';
-import * as Logger from '../util/logger';
-import { $ } from '../util/query-selector';
+import { $, $all } from '../util/query-selector';
+
+const svgClassToResultMap = new StrMap<ReviewResult>({
+  'octicon-check': 'approved',
+  'octicon-x': 'requestedChanges',
+  'octicon-request-changes': 'requestedChanges',
+  'octicon-comment': 'leftComments',
+  'octicon-primitive-dot': 'unreviewed',
+});
 
 export async function fetchReviews(url: string): Promise<ReviewCollection> {
   const page = h('html', {
@@ -11,33 +18,17 @@ export async function fetchReviews(url: string): Promise<ReviewCollection> {
     },
   });
 
-  const reviews: Review[] = [];
-  Array.from($<HTMLSpanElement>(page, '.discussion-sidebar-item .css-truncate')!.children).forEach((ele) => {
-    let reviewer: Reviewer;
-    try {
-      const icon = $<HTMLImageElement>(ele, 'img')!;
-      reviewer = {
-        name: icon.alt.slice(1), // 先頭の "@" を除く
-        iconUrl: icon.src,
-      };
-    } catch (_) {
-      Logger.debug('レビュワーの取得に失敗したため、レビュワー0人として扱う', { url, ele });
-      return;
-    }
+  const reviews = $all<HTMLSpanElement>(page, '[data-assignee-name]').map<Review>(($span) => {
+    const reviewer = {
+      name: $span.dataset.assigneeName!,
+      iconUrl: $<HTMLImageElement>($span, 'img')!.src,
+    };
 
-    let status: ReviewResult;
-    const classes = $(ele, '.reviewers-status-icon svg')!.classList;
-    if (classes.contains('octicon-check')) {
-      status = 'approved';
-    } else if (classes.contains('octicon-x')) {
-      status = 'requestedChanges';
-    } else if (classes.contains('octicon-primitive-dot')) {
-      status = 'unreviewed';
-    } else {
-      status = 'leftComments';
-    }
+    const $svg = $($span.nextElementSibling!, '.reviewers-status-icon svg')!;
+    const svgClass = Array.from($svg.classList).find((a) => a.startsWith('octicon-'))!;
+    const result = lookup(svgClass, svgClassToResultMap).getOrElse('unreviewed');
 
-    reviews.push({ reviewer, result: status });
+    return { result, reviewer };
   });
 
   return new ReviewCollection(reviews);
