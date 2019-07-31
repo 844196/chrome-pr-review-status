f4d2ece3daffca3c6af70c20a221d9a3ca3a91de
Masaya Takeda <844196@gmail.com>
2019-08-01 05:06:04 +0900

    remove unused type definitions

--- src/type.d.ts
+++ src/type.d.ts
@@ -1,24 +1,2 @@
 // SEE: webpack.config.js -- DefinePlugin
 declare var ENVIRONMENT: 'production' | 'development';
-
-type SubType<T, U> = Pick<T, { [K in keyof T]: T[K] extends U ? K : never }[keyof T]>;
-
-declare module 'octicons' {
-  interface Octicon {
-    toSVG(): string;
-  }
-
-  // 必要な分だけ定義
-  let icons: {
-    comment: Octicon;
-    x: Octicon;
-    check: Octicon;
-    'primitive-dot': Octicon;
-    fold: Octicon;
-    unfold: Octicon;
-  };
-
-  export = icons;
-}
-
-declare module 'dom-chef/react';
